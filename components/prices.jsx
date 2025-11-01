2d13e0345e9fb052df829c4fc5317dc232304ba5
Владислав <kay.offical@MacBook-Air-Vladislav.local>
2025-11-01 11:50:50 +0700

    3 блока

--- components/prices.jsx
+++ components/prices.jsx
@@ -1,7 +1,27 @@
 export default function Prices() {
   return (
     <div className="prices">
-      <span className="priceText">Что по ценам?</span>
+      <span className="priceText">
+        Что по <br /> ценам?
+      </span>
+      <div className="airDelivery">
+        <img src="img/plane.svg" alt="" />
+        <div className="textAirDelivery">
+          <span>Авиаперевозка</span>
+          <span>Вес груза от 30 кг</span>
+          <span>От 9,5$ за 1 кг</span>
+          <span>От 7 дней</span>
+        </div>
+      </div>
+      <div className="carDelivery">
+        <img src="img/roadDelivery.svg" alt="" />
+        <div className="textCarDelivery">
+          <span>Автоперевозка</span>
+          <span>Вес груза от 100 к</span>
+          <span>От 3,5$ за 1 кг</span>
+          <span>От 14 дней</span>
+        </div>
+      </div>
     </div>
   );
 }
